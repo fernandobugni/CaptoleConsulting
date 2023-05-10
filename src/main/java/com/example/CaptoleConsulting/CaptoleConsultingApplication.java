f835ff4c3bc1b98117bf2a09f88d84954fb31fa2
Fernando Bugni <fernando.bugni@gmail.com>
2023-05-09 21:16:46 -0300

    Adding project structure

---
+++ src/main/java/com/example/CaptoleConsulting/CaptoleConsultingApplication.java
@@ -0,0 +1,13 @@
+package com.example.CaptoleConsulting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaptoleConsultingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaptoleConsultingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CaptoleConsulting/CaptoleConsultingApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.CaptoleConsulting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaptoleConsultingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
