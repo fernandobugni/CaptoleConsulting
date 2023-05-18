46e03568dd4d58c01f1e15344a488c0c150ed0f4
Fernando Bugni <fernando.bugni@gmail.com>
2023-05-18 01:13:05 -0300

    Adding first test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -15,7 +15,7 @@ create table Prices(
 	price_list int,
 	product_id int,
 	priority int,
-	price numeric(2),
+	price NUMERIC (5, 2),
 	curr varchar not null,
 	foreign key (brand_id) references Brands(brand_id),
 	foreign key (product_id) references Products(product_id)
@@ -24,7 +24,7 @@ create table Prices(
 insert into Brands(brand_id, brand_name) values (1, 'ZARA');
 insert into Products(product_id, product_name) values (35455, 'Producto 35455');
 insert into Prices(brand_id, start_date, end_date, price_list, product_id, priority, price, curr) values
-(1, '2020-06-14 00:00+00'::timestamp, '2020-12-31 23:59+00'::timestamp, 1, 35455, 0, 35.50, 'EUR'),
-(1, '2020-06-14 15:00+00'::timestamp, '2020-06-14 18:30+00'::timestamp, 2, 35455, 1, 25.45, 'EUR'),
-(1, '2020-06-15 00:00+00'::timestamp, '2020-06-15 11:00+00'::timestamp, 3, 35455, 1, 30.50, 'EUR'),
-(1, '2020-06-15 16:00+00'::timestamp, '2020-12-31 23:59+00'::timestamp, 4, 35455, 1, 38.95, 'EUR');
+(1, '2020-06-14 00:00:00'::timestamp, '2020-12-31 23:59:59'::timestamp, 1, 35455, 0, 35.50, 'EUR'),
+(1, '2020-06-14 15:00:00'::timestamp, '2020-06-14 18:30:00'::timestamp, 2, 35455, 1, 25.45, 'EUR'),
+(1, '2020-06-15 00:00:00'::timestamp, '2020-06-15 11:00:00'::timestamp, 3, 35455, 1, 30.50, 'EUR'),
+(1, '2020-06-15 16:00:00'::timestamp, '2020-12-31 23:59:59'::timestamp, 4, 35455, 1, 38.95, 'EUR');
--- src/test/java/com/example/CaptoleConsulting/CaptoleConsultingApplicationTests.java
+++ src/test/java/com/example/CaptoleConsulting/CaptoleConsultingApplicationTests.java
@@ -1,12 +1,59 @@
 package com.example.CaptoleConsulting;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.hamcrest.CoreMatchers.is;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = CaptoleConsultingApplication.class)
+@AutoConfigureMockMvc
 class CaptoleConsultingApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void example() throws Exception{
+
+		mvc.perform(get("/getPrices")
+				.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+	}
+
+	@Test
+	void example1() throws Exception{
+
+		String brand_id = "1";
+		String product_id = "35455";
+		LocalDateTime datetime = LocalDateTime.of(2020, 06, 14, 10, 0, 0);
+		String datetimestr = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(datetime);
+
+		mvc.perform(get("/getPriceFromData/brand/{brand_id}/product/{product_id}?localDateTime={datetimestr}", brand_id, product_id, datetimestr)
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.price").value("35.5"));
 	}
 }
